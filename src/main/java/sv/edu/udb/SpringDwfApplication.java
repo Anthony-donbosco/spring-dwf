6fc594f1114f6c100b807586b6220650b394bc28
Anthony-donbosco <anthony.3719.donbosco@gmail.com>
2026-08-29 23:36:02 -0600

    Proyecto Spring DWF

---
+++ src/main/java/sv/edu/udb/SpringDwfApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.udb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDwfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDwfApplication.class, args);
+	}
+
+}
